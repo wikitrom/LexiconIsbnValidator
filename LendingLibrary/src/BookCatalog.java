b27749721988aff18930d5fe0f214187ace3e4a4
Mats Bergtrom <srctrom@gmail.com>
2018-02-19 10:34:04 +0100

    Add collection class BookCatalog
    Add a new class that handles collections of books.

--- LendingLibrary/src/BookCatalog.java
+++ LendingLibrary/src/BookCatalog.java
@@ -1,5 +1,27 @@
 
 public class BookCatalog {
 
-	
+	private Book[] bookArray = new Book[100];
+	private int nextPosition = 0;
+
+	// constructors
+
+	// public methods
+	public Book[] getBookArray() {
+		return bookArray;
+	}
+
+	public void addBook(Book newBook) {
+		bookArray[nextPosition] = newBook;
+		nextPosition++;
+	}
+
+	public Book findBook(String title) {
+		for (int i = 0; i < nextPosition; i++) {
+			if (bookArray[i].getTitle().equalsIgnoreCase(title)) {
+				return bookArray[i];
+			}
+		}
+		return null;
+	}
 }
--- LendingLibrary/src/Main.java
+++ LendingLibrary/src/Main.java
@@ -3,21 +3,31 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Book book1 = new Book(1, "Introduction to Java", "Mats Bergström", "009955551112");		
-		Book book2 = new Book(2, "Introduction to C", "Mats Bergström", "009955551113");
-		Book book3 = new Book(1234567, "Dummy book", "Mats Bergström", "009955551114");
-		UI ui = new UI();
-		ui.printHeader();
-		ui.printBook(book1);
-		ui.printBook(book2);
-		ui.printBook(book3);
+		BookCatalog bookCatalog = new BookCatalog();
 		
+		Book book1 = new Book(1, "Introduction to Java", "Mats BergstrÃ¶m", "009955551112");		
+		Book book2 = new Book(2, "Introduction to C", "Mats BergstrÃ¶m", "009955551113");
+		Book book3 = new Book(3, "Dummy book", "Mats BergstrÃ¶m", "009955551114");
+		UI ui = new UI();
+		
+		bookCatalog.addBook(book1);
+		bookCatalog.addBook(book2);
+		bookCatalog.addBook(book3);
+				
+		ui.printHeader();
+		ui.printBookCatalog(bookCatalog.getBookArray());
+
+		
+		Book foundBook = bookCatalog.findBook("dummy book");
+		if (foundBook != null) {
+			System.out.println("Found the book titled \"" + foundBook.getTitle() + "\"");
+		}
 	}
 
 }
 
-//		Customer henrik = new Customer("Mr", "Henrik", "Åman", "unknown", "+46555555555", "unknown", GenderType.MALE);
-//		Customer mats = new Customer("Mr", "Mats", "Bergström");
+//		Customer henrik = new Customer("Mr", "Henrik", "ï¿½man", "unknown", "+46555555555", "unknown", GenderType.MALE);
+//		Customer mats = new Customer("Mr", "Mats", "Bergstrï¿½m");
 //		Customer tove = new Customer("Miss", "Tove", "Hult");		
 //		
 //		System.out.println("Customer number: " + mats.getCustomerId() +
