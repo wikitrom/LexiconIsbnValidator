799dd49052b56969e51877ef8bf0d53377595d8a
Mats Bergstroem <hackingtrom@gmail.com>
2018-02-27 10:32:03 +0100

    TDD: Refacor code into multiple methods

--- TestDrivenDevelopmentISBNValidator/src/se/mibis/isbntools/ValidateISBN.java
+++ TestDrivenDevelopmentISBNValidator/src/se/mibis/isbntools/ValidateISBN.java
@@ -2,53 +2,59 @@ package se.mibis.isbntools;
 
 public class ValidateISBN {
 
+	private static final int SHORT_ISBN_LENGTH = 10;
+	private static final int SHORT_ISBN_MULTIPLIER = 11;
+	private static final int LONG_ISBN_LENGTH = 13;
+	private static final int LONG_ISBN_MULTIPLIER = 10;
+
 	public boolean checkISBN(String isbn) throws NumberFormatException {
-
-		int total = 0;
-
 		isbn = isbn.replaceAll("-", ""); // trim ISBN-13 numbers
 
-		if (isbn.length() == 13) {   // 13-digit ISBN
-			int digit;
-//			int checksum;
-
-//			for (int i = 0; i < 12; i++) {			
-			for (int i = 0; i < 13; i++) {
-
-				if (Character.isDigit(isbn.charAt(i))) {
-					digit = Character.getNumericValue(isbn.charAt(i));
-					total += (i % 2 == 0) ? digit * 1 : digit * 3;
-				} else {
-					throw new NumberFormatException("Provided ISBN has non-numerical character: " + isbn.charAt(i));
-				}
-			}
-//			checksum = 10 - (total % 10);
-//			if (checksum == 10)
-//				checksum = 0;
-//			return (checksum == Character.getNumericValue(isbn.charAt(12)));
-			return (total % 10== 0) ? true :false;  
-			
-		} else if (isbn.length() == 10) {
-			for (int i = 0; i < 10; i++) {
-
-				if (Character.isDigit(isbn.charAt(i))) {
-					total += (Character.getNumericValue(isbn.charAt(i)) * (10 - i));
-				} else if ((i == 9) && (isbn.charAt(i) == 'X')) {
-					total += 10;
-				} else {
-					throw new NumberFormatException("Provided ISBN has non-numerical character: " + isbn.charAt(i));
-				}
-			}
-
-			if ((total % 11) == 0) {
-				return true;
-			} else {
-				return false;
-			}
-
-		} else {
-			throw new NumberFormatException("An ISBN number must have 10 or 13 digits.");
+		if (isbn.length() == LONG_ISBN_LENGTH) { // 13-digit ISBN
+			return isValidLongISBN(isbn);
+		} else if (isbn.length() == SHORT_ISBN_LENGTH) {
+			return isValidShortISBN(isbn);
 		}
+		throw new NumberFormatException("An ISBN number must have 10 or 13 digits.");
+
+	}
+
+	private boolean isValidShortISBN(String isbn) {
+		int total = 0;
+
+		for (int i = 0; i < SHORT_ISBN_LENGTH; i++) {
+			if (Character.isDigit(isbn.charAt(i))) {
+				total += (Character.getNumericValue(isbn.charAt(i)) * (SHORT_ISBN_LENGTH - i));
+			} else if ((i == 9) && (isbn.charAt(i) == 'X')) {
+				total += 10;
+			} else {
+				throw new NumberFormatException("Provided ISBN has non-numerical character: " + isbn.charAt(i));
+			}
+		}
+
+		return ((total % SHORT_ISBN_MULTIPLIER) == 0);
+	}
+
+	private boolean isValidLongISBN(String isbn) {
+		int total = 0;
+		int digit = 0;
+		// int checksum;
+
+		// for (int i = 0; i < 12; i++) {
+
+		for (int i = 0; i < LONG_ISBN_LENGTH; i++) {
+			if (Character.isDigit(isbn.charAt(i))) {
+				digit = Character.getNumericValue(isbn.charAt(i));
+				total += (i % 2 == 0) ? digit * 1 : digit * 3;
+			} else {
+				throw new NumberFormatException("Provided ISBN has non-numerical character: " + isbn.charAt(i));
+			}
+		}
+		// checksum = 10 - (total % 10);
+		// if (checksum == 10)
+		// checksum = 0;
+		// return (checksum == Character.getNumericValue(isbn.charAt(12)));
+		return (total % LONG_ISBN_MULTIPLIER == 0);
 	}
 
 }
